9659ddacf1c5438449fa2f6bc1b614dfef78bac3
bernardomenendez <bernardo.menendez@atrapalo.com>
2017-03-04 15:08:03 +0100

    Change namespace

--- src/Exception/MockException.php
+++ src/Exception/MockException.php
@@ -3,7 +3,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2017 ATRAPALO
+ * Copyright (c) 2017 Bernardo Secades
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
